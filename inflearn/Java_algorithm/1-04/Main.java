a35c2380425a04353de17a2f06c149fb84f43a0d
anifilm <anifilm02@gmail.com>
2021-04-08 09:32:55 +0900

    자료 추가

---
+++ inflearn/Java_algorithm/1-04/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String solution(String str) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+
+        System.out.println(solution(input));
+    }
+}
