b9092183fcbbda87f0e369b9ec9d71eefdabca6a
anifilm <anifilm02@gmail.com>
2021-04-11 16:18:27 +0900

    study java_algorithm 2021-04-11

--- inflearn/java/1-11/Main.java
+++ inflearn/java/1-11/Main.java
@@ -3,7 +3,17 @@ import java.util.*;
 public class Main {
     public static String solution(String str) {
         String answer = "";
-
+        str += " ";
+        int count = 1;
+        for (int i = 0; str.charAt(i) != ' '; i++) {
+            char tmp = str.charAt(i);
+            if (tmp != str.charAt(i + 1)) {
+                answer += tmp;
+                if (count > 1) answer += count;
+                count = 1;
+            }
+            else count++;
+        }
         return answer;
     }
 
