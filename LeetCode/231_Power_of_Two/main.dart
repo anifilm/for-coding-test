585c7b41391c21535a2dcaab83bc45b7786e135b
anifilm <anifilm02@gmail.com>
2021-04-14 00:18:21 +0900

    Update main.dart

--- LeetCode/231_Power_of_Two/main.dart
+++ LeetCode/231_Power_of_Two/main.dart
@@ -1,11 +1,6 @@
 bool solution(int n) {
     while(n > 0 && n%2 == 0) {
-        n = (n/2).toInt(); // 묵시적 형변환을 지원하지 않는다.
-        // Convert double to int (다음과 같이 int형으로 형 변환 필요)
-        // toInt() and truncate()
-        // round()
-        // ceil()
-        // floor()
+        n ~/= 2; // ~/ (정수형 나눗셈)
     }
     return n == 1 ? true : false;
 }
