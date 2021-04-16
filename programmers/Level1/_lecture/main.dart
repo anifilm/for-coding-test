5c3cca78ac2d81c969b2b851b0e291dc5a0462b0
anifilm <anifilm02@gmail.com>
2021-04-17 02:22:45 +0900

    정보 수정

--- programmers/Level1/_lecture/main.dart
+++ programmers/Level1/_lecture/main.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 String solution(List<int> numbers) {
-  String answer = '';
+  String answer = 'test';
   return answer;
 }
 
 void main() {
   var numbers = <int>[];
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < 3; i++) {
     numbers.add(int.parse(stdin.readLineSync()!));
   }
   print(solution(numbers));
