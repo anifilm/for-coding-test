f18bc5a4888e53859daac86ecaa5d98942cb0e6e
anifilm <anifilm02@gmail.com>
2021-04-29 07:30:19 +0900

    Update main.dart

--- LeetCode/231_Power-of-Two/main.dart
+++ LeetCode/231_Power-of-Two/main.dart
@@ -1,14 +1,13 @@
 bool solution(int n) {
-    while(n > 0 && n%2 == 0) {
-        n ~/= 2; // ~/ (정수형 나눗셈)
-    }
-    return n == 1 ? true : false;
+  while (n > 0 && n % 2 == 0) {
+    n ~/= 2; // ~/ (정수형 나눗셈)
+  }
+  return n == 1 ? true : false;
 }
 
 main() {
-    print(solution(1));
-    print(solution(16));
-    print(solution(4));
-    print(solution(8));
+  print(solution(1));
+  print(solution(16));
+  print(solution(4));
+  print(solution(8));
 }
-
