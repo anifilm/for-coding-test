2a441a84fbbeff1384efbb5b5dce92bfc44d4842
anifilm <anifilm02@gmail.com>
2021-04-29 13:33:51 +0900

    자료 정리

--- Baekjoon/_StarterPack/__run_kotilin__.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%1" == "" kotlinc main.kt -include-runtime -d main.jar
-if NOT "%1" == "" kotlinc %1 -include-runtime -d main.jar
-java -jar main.jar
-pause
-del *.jar
--- checkio/typescript/Elementary/First_Word_(simplified).ts
+++ checkio/typescript/Elementary/First_Word_(simplified).ts
@@ -2,7 +2,7 @@ import assert from "assert";
 
 function firstWord(text: string): string {
     // your code here
-    return text.split(' ')[0] ;
+    return text.split(' ')[0];
 }
 
 console.log('Example:');
