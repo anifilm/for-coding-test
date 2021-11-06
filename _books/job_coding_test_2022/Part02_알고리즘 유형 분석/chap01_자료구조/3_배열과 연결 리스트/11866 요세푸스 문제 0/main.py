cb15b14c040b8f3748d00f3ad414c624d1953f85
anifilm <anifilm02@gmail.com>
2021-11-06 16:56:56 +0900

    Update main.py

--- _books/job_coding_test_2022/Part02_알고리즘 유형 분석/chap01_자료구조/3_배열과 연결 리스트/11866 요세푸스 문제 0/main.py
+++ _books/job_coding_test_2022/Part02_알고리즘 유형 분석/chap01_자료구조/3_배열과 연결 리스트/11866 요세푸스 문제 0/main.py
@@ -4,25 +4,21 @@ a = [str(i + 1) for i in range(n)]
 #print(a)
 
 result = []
-t = k - 1
+t = 0
 
-while 1:
-    if len(a) <= k - 1:
-        result.extend(a)
-        break
+for _ in range(n):
+    t += k - 1
+    t %= len(a)
     result.append(a.pop(t))
     #print(result)
     #print(a)
-    t += k - 1
-    if t >= len(a):
-        t -= len(a)
 
 #print(result)
 print("<" + ", ".join(result) + ">")
 
 # 예제1 은 만족하지만 제출시 틀림
 # 길이가 작다면 붙이고 종료 부분 수정 필요
-# 순서대로 K번 세어서 제거해야 함
+# 순서대로 K번 세어서 제거해야 함 (수정 완료)
 
 # 반례 5 4 입력시
-# <4, 3, 5, 1, 2>
+# <4, 3, 5, 2, 1>
