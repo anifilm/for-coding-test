75e2644c6121f7dfd75500f426389462d8bc5653
anifilm <anifilm02@gmail.com>
2022-08-24 22:26:03 +0900

    Update The_Most_Numbers.py

--- checkio/python/Elementary/The_Most_Numbers.py
+++ checkio/python/Elementary/The_Most_Numbers.py
@@ -4,15 +4,16 @@ def checkio(*args):
     else:
         return 0
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
+
 if __name__ == '__main__':
     def almost_equal(checked, correct, significant_digits):
         precision = 0.1 ** significant_digits
         return correct - precision < checked < correct + precision
         
-    print('Example:')
+    print("Example:")
     print(checkio(1, 2, 3))
     
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert almost_equal(checkio(1, 2, 3), 2, 3), "3-1=2"
     assert almost_equal(checkio(5, -5), 10, 3), "5-(-5)=10"
     assert almost_equal(checkio(10.2, -2.2, 0, 1.1, 0.5), 12.4, 3), "10.2-(-2.2)=12.4"
