51e6a2294fd4de61a347e44cf904e39b7c613522
anifilm <anifilm02@gmail.com>
2023-02-26 20:45:52 +0900

    study java 2023-02-26

---
+++ _books/java-coding-interview/src/chap06/abstraction/abscreaction_via_interface/Car.java
@@ -0,0 +1,9 @@
+package chap06.abstraction.abscreaction_via_interface;
+
+public interface Car {
+    public void sppedUp();
+    public void slowDown();
+    public void turnRight();
+    public void turnLeft();
+    public String getCarType();
+}
---
+++ _books/java-coding-interview/src/chap06/abstraction/abscreaction_via_interface/ElectricCar.java
@@ -0,0 +1,34 @@
+package chap06.abstraction.abscreaction_via_interface;
+
+public class ElectricCar implements Car {
+    private final String carType;
+
+    public ElectricCar(String carType) {
+        this.carType = carType;
+    }
+
+    @Override
+    public void speedUp() {
+        System.out.println("Speed up the electric car");
+    }
+
+    @Override
+    public void slowDown() {
+        System.out.println("Slow down the electric car");
+    }
+
+    @Override
+    public void turnRight() {
+        System.out.println("Turn right the electric car");
+    }
+
+    @Override
+    public void turnLeft() {
+        System.out.println("Turn left the electric car");
+    }
+
+    @Override
+    public String getCarType() {
+        return this.carType;
+    }
+}
