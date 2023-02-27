3122b1117d035cf0048ea25040e3e4c6d8f967c1
anifilm <anifilm02@gmail.com>
2023-02-27 21:30:41 +0900

    study java 2023-02-27

---
+++ _books/java-coding-interview/src/chap06/association/Address.java
@@ -0,0 +1,27 @@
+package chap06.association;
+
+public class Address {
+    private String city;
+    private String zip;
+
+    public Address(String city, String zip) {
+        this.city = city;
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
---
+++ _books/java-coding-interview/src/chap06/association/Main.java
@@ -0,0 +1,20 @@
+package chap06.association;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Andrei");
+        Person p2 = new Person("Marin");
+        Address a1 = new Address("Banesti", "107050");
+        Address a2 = new Address("Bucuresti", "229344");
+
+        // 메인 메서드에서의 두 클래스 간 연관(Association) 관계
+        System.out.println(p1.getName() + " lives at address "
+                + a2.getCity() + ", " + a2.getZip()
+                + " but it also has an address at "
+                + a1.getCity() + ", " + a1.getZip());
+        System.out.println(p2.getName() + " lives at address "
+                + a1.getCity() + ", " + a1.getZip()
+                + " but it also has an address at "
+                + a2.getCity() + ", " + a2.getZip());
+    }
+}
