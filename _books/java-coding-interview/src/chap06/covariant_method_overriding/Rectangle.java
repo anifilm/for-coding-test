17034cbf857d325df3c0bd0ea5b2efb7cf5d1dfd
anifilm <anifilm02@gmail.com>
2023-03-18 16:15:54 +0900

    study java 2023-03-18

---
+++ _books/java-coding-interview/src/chap06/covariant_method_overriding/Rectangle.java
@@ -0,0 +1,25 @@
+package chap06.covariant_method_overriding;
+
+public class Rectangle implements Cloneable {
+    private final int height;
+    private final int width;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    protected Rectangle clone() throws CloneNotSupportedException {
+        Rectangle clone = (Rectangle) super.clone();
+        return clone;
+    }
+}
---
+++ _books/java-coding-interview/src/chap06/java8_default_static_methods/Vehicle.java
@@ -0,0 +1,15 @@
+package chap06.java8_default_static_methods;
+
+public interface Vehicle {
+    public void speedUp();
+    public void slowDown();
+
+    default double computeConsumption(int fuel, int distance, int horsePower) {
+        // 연비 계산을 가장한 임의의 계산식입니다.
+        return Math.random() * 10d;
+    }
+
+    static void description() {
+        System.out.println("This interface control steam, petrol and electric cars");
+    }
+}
